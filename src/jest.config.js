ef3b5833f4671915707eea4c3f8916d1a835454c
numb86 <16703337+numb86@users.noreply.github.com>
2019-07-30 23:21:56 +0900

    chore: テストの設定ファイルを移動した。

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/test.config.js'],
   preset: 'ts-jest/presets/js-with-babel',
-  testMatch: ['<rootDir>/src/**/__tests__/**.test.(ts|tsx)'],
+  testMatch: ['<rootDir>/**/__tests__/**.test.(ts|tsx)'],
   globals: {
     'ts-jest': {
       babelConfig: true,
